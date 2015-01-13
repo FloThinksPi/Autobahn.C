89d83498ffdf1d580ea39d896af1600711fea8cd
FloThinksPi <flo@myprivatemail.de>
2015-01-13 12:13:25 +0100

    Moved to Netbeans

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: flo
+ *
+ * Created on 13. Januar 2015, 12:10
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
