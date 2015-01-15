9cb2622249ef6488fe4187ed1556302ce1e33e40
Florian Braun <flo@myprivatemail.de>
2015-01-15 14:30:30 +0100

    cleanup

---
+++ src/main.c
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.c
+ * Author: flo
+ *
+ * Created on 13. Januar 2015, 12:10
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+
+int main(int argc, char** argv) {
+ 
+    printf("Hello world\n");
+ 
+    return (EXIT_SUCCESS);
+    
+}
+
---
+++ src/main.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.h
+ * Author: flo
+ *
+ * Created on 13. Januar 2015, 12:11
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MAIN_H */
+
