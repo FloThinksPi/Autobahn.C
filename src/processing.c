8103802db08bb269385c11b13f65379b35d5f22f
FloThinksPi <flo@myprivatemail.de>
2015-01-30 15:33:07 +0100

    Alg ist Fertig , Funktioniert in allen fällen
    TODO:
    Ausgabe in Tabellenform schreiben
    Eine Ausgabe die nur einen Bestimmten Pfad (Start -> Ziel) anzeigt
    Einlesen von Daten aus dbio.c
    Integration ins Menu , Mit parameter fähikkeit der suchfunktion

--- src/processing.c
+++ src/processing.c
@@ -6,79 +6,77 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-// Number of vertices in the graph
-#define AnzahlKnoten 4
 
 
-struct Karte {
-    char placeholder;
-    struct Knoten *Knoten[AnzahlKnoten];//TODO Ohne Präprozessor arbeiten , alles zu pointern machen
-};
+//Anzahl der Knoten
+int AnzahlKnoten = NULL;
+
 
 struct Knoten {
+    int ID;
     char *Name;
     bool besucht;
+    struct Knoten *knotenZurueck;
     double entfernungZumUrsprung;
     int numWege;
     struct Wege *Wege[20];
 };
 
 struct Wege {
-    struct Knoten *von;
+    struct Knoten *von; //TODO von nicht nötig da info in dem vorhandensein des eintrags beim knoten steckt -> löschen
     struct Knoten *nach;
     double laenge;
 };
 
 
-struct Karte ErstelleWegBidirektional(struct Karte meineKarte,int Knoten1,int Knoten2,int leange){
+void ErstelleWegBidirektional(struct Knoten *meineKnoten[],int Knoten1,int Knoten2,int leange){
 
-    int numWegezuKnoten1 = meineKarte.Knoten[Knoten1]->numWege;
-    int numWegezuKnoten2 = meineKarte.Knoten[Knoten2]->numWege;
+    int numWegezuKnoten1 = meineKnoten[Knoten1]->numWege;
+    int numWegezuKnoten2 = meineKnoten[Knoten2]->numWege;
 
     //TODO Püfen ob weg schon existiert
 
     struct Wege *W1 =  malloc(sizeof(struct Wege));
     struct Wege *W2 =  malloc(sizeof(struct Wege));
 
-    meineKarte.Knoten[Knoten1]->Wege[numWegezuKnoten1]=W1;
-    meineKarte.Knoten[Knoten2]->Wege[numWegezuKnoten2]=W2;
+    meineKnoten[Knoten1]->Wege[numWegezuKnoten1]=W1;
+    meineKnoten[Knoten2]->Wege[numWegezuKnoten2]=W2;
 
-    meineKarte.Knoten[Knoten1]->Wege[numWegezuKnoten1]->von=meineKarte.Knoten[Knoten1];
-    meineKarte.Knoten[Knoten1]->Wege[numWegezuKnoten1]->nach=meineKarte.Knoten[Knoten2];
-    meineKarte.Knoten[Knoten1]->Wege[numWegezuKnoten1]->laenge=leange;
+    meineKnoten[Knoten1]->Wege[numWegezuKnoten1]->von=meineKnoten[Knoten1];
+    meineKnoten[Knoten1]->Wege[numWegezuKnoten1]->nach=meineKnoten[Knoten2];
+    meineKnoten[Knoten1]->Wege[numWegezuKnoten1]->laenge=leange;
 
-    meineKarte.Knoten[Knoten2]->Wege[numWegezuKnoten2]->von=meineKarte.Knoten[Knoten2];
-    meineKarte.Knoten[Knoten2]->Wege[numWegezuKnoten2]->nach=meineKarte.Knoten[Knoten1];
-    meineKarte.Knoten[Knoten2]->Wege[numWegezuKnoten2]->laenge=leange;
+    meineKnoten[Knoten2]->Wege[numWegezuKnoten2]->von=meineKnoten[Knoten2];
+    meineKnoten[Knoten2]->Wege[numWegezuKnoten2]->nach=meineKnoten[Knoten1];
+    meineKnoten[Knoten2]->Wege[numWegezuKnoten2]->laenge=leange;
 
-    meineKarte.Knoten[Knoten1]->numWege++;
-    meineKarte.Knoten[Knoten2]->numWege++;
+    meineKnoten[Knoten1]->numWege++;
+    meineKnoten[Knoten2]->numWege++;
 
-    return meineKarte;
+
 }
 
-struct Karte setzeKnotenName(struct Karte meineKarte, int KnotenNummer, char *KnotenName){
+void setzeKnotenName(struct Knoten *meineKnoten[], int KnotenNummer, char *KnotenName){
 
 
     struct Knoten *K =  malloc(sizeof(struct Knoten));
 
-    meineKarte.Knoten[KnotenNummer]=K;
-    meineKarte.Knoten[KnotenNummer]->Name=KnotenName;
+    meineKnoten[KnotenNummer]=K;
+    meineKnoten[KnotenNummer]->Name=KnotenName;
+    meineKnoten[KnotenNummer]->ID=KnotenNummer;
 
-    return meineKarte;
+
 }
 
 
-double gibWegLaenge(struct Karte meineKarte,int u,int v){
+double gibWegLaenge(struct Knoten *meineKnoten[],int u,int v){
 
     int i=0;
-    while(i < meineKarte.Knoten[u]->numWege){
+    while(i < meineKnoten[u]->numWege){
 
 
-
-
-        if(meineKarte.Knoten[u]->Wege[i]->nach==meineKarte.Knoten[v]){
-            return meineKarte.Knoten[u]->Wege[i]->laenge;
+        if(meineKnoten[u]->Wege[i]->nach==meineKnoten[v]){
+            return meineKnoten[u]->Wege[i]->laenge;
         }
 
         i++;
@@ -89,54 +87,44 @@ double gibWegLaenge(struct Karte meineKarte,int u,int v){
 }
 
 
-// A utility function to find the vertex with minimum distance value, from
-// the set of vertices not yet included in shortest path tree
-double minDistance(struct Karte meineKarte)
+
+//Errechnet den am kürzesten entfernten nächsten knoten aus allen noch nicht besuchten knoten.
+//Wählt somit den "Weg" dergegangen wird.
+double minDistance(struct Knoten *meineKnoten[])
 {
     // Initialize min value
     double min = INT_MAX, min_index ;
 
     for (int v = 0; v < AnzahlKnoten; v++) {
-        if (meineKarte.Knoten[v]->besucht == false && meineKarte.Knoten[v]->entfernungZumUrsprung <= min) {
-            min = meineKarte.Knoten[v]->entfernungZumUrsprung;
+        if (meineKnoten[v]->besucht == false && meineKnoten[v]->entfernungZumUrsprung <= min) {
+            min = meineKnoten[v]->entfernungZumUrsprung;
             min_index = v;
         }
 
     }
 
-
-    //printf("%1.0f-->%1.0f(%1.0f)\n",keepPreeviousNode,min_index,min);
     return min_index;
 }
 
-// A utility function to print the constructed distance array
-int printSolution(struct Karte meineKarte)
-{
-    printf("Knoten   Distance from Source\n");
-    for (int i = 0; i < AnzahlKnoten; i++)
-        printf("%s \t\t %f\n", meineKarte.Knoten[i]->Name, meineKarte.Knoten[i]->entfernungZumUrsprung);
-}
 
-// Funtion that implements Dijkstra's single source shortest path algorithm
-// for a graph represented using adjacency matrix representation
-void dijkstra(struct Karte meineKarte, int src)
+//Hier wird der Algorythmus gestartet und die gegangenen Wege Gespeichert.
+void dijkstra(struct Knoten *meineKnoten[], int src)
 {
 
 
-    // Distance of source vertex from itself is always 0
-    meineKarte.Knoten[src]->entfernungZumUrsprung = 0;
+    //Entfernung = 0 da der startknoten zu sich selbst nicht entfernt ist
+    meineKnoten[src]->entfernungZumUrsprung = 0;
 
     // Find shortest path for all vertices (V-1 Möglich da der letzte wert automatisch fest steht , aber nicht gemacht da sonst der letzte wert nicht ausgegeben wird)
-    for (int count = 0; count < AnzahlKnoten; count++)
+    for (int count = 0; count < AnzahlKnoten-1; count++)
     {
         int u=NULL;
 
-        // Pick the minimum distance vertex from the set of vertices not
-        // yet processed. u is always equal to src in first iteration.
-        u = minDistance(meineKarte);
+        //U = immer src beim ersten mal , weitere info siehe funktion
+        u = minDistance(meineKnoten);
 
         // Mark the picked vertex as processed
-        meineKarte.Knoten[u]->besucht = true;
+        meineKnoten[u]->besucht = true;
 
 
 
@@ -147,9 +135,10 @@ void dijkstra(struct Karte meineKarte, int src)
             // u to v, and total weight of path from src to  v through u is
             // smaller than current value of dist[v]
 
-            if(!meineKarte.Knoten[v]->besucht && gibWegLaenge(meineKarte,u,v) && meineKarte.Knoten[u]->entfernungZumUrsprung != INT_MAX
-                    && meineKarte.Knoten[u]->entfernungZumUrsprung + gibWegLaenge(meineKarte,u,v) < meineKarte.Knoten[v]->entfernungZumUrsprung){
-                meineKarte.Knoten[v]->entfernungZumUrsprung = meineKarte.Knoten[u]->entfernungZumUrsprung + gibWegLaenge(meineKarte,u,v);
+            if(!meineKnoten[v]->besucht && gibWegLaenge(meineKnoten,u,v) && meineKnoten[u]->entfernungZumUrsprung != INT_MAX
+                    && meineKnoten[u]->entfernungZumUrsprung + gibWegLaenge(meineKnoten,u,v) < meineKnoten[v]->entfernungZumUrsprung){
+                meineKnoten[v]->entfernungZumUrsprung = meineKnoten[u]->entfernungZumUrsprung + gibWegLaenge(meineKnoten,u,v);
+                meineKnoten[v]->knotenZurueck=meineKnoten[u];
             }
 
 
@@ -159,48 +148,103 @@ void dijkstra(struct Karte meineKarte, int src)
 
     }
 
-    //TODO print the constructed distance array
-
-    printSolution(meineKarte);
-
 }
 
-// driver program to test above function
-int start()
+
+// Gibt Alle möglichen Ziele mit gegangenem Weg aus
+int printAllPaths(struct Knoten *meineKnoten[],int StartKnoten)
+{
+    for (int i = 0; i < AnzahlKnoten; i++){
+        int v=i;
+        char *Verlauf[AnzahlKnoten];
+        int CountWays=0;
+        while(meineKnoten[v]->knotenZurueck->Name!=NULL){
+
+            char *buffer=malloc(sizeof(LONG_MAX));
+
+            sprintf(buffer,"%s -> %s (%.2f)",meineKnoten[v]->knotenZurueck->Name,meineKnoten[v]->Name, gibWegLaenge(meineKnoten, v, meineKnoten[v]->knotenZurueck->ID));
+            v=meineKnoten[v]->knotenZurueck->ID;
+            Verlauf[CountWays]= malloc(sizeof(LONG_MAX));
+            Verlauf[CountWays]=buffer;
+
+            CountWays++;
+
+        }
+
+        for(int u=CountWays;u>0;u--){
+                printf("%s | ",Verlauf[u-1]);
+        }
+
+        if(meineKnoten[i]->ID!=meineKnoten[StartKnoten]->ID){
+            if(meineKnoten[i]->entfernungZumUrsprung== INT_MAX){
+                printf("\"%s\" ist nicht Erreichbar\n",meineKnoten[i]->Name);
+            }else {
+                printf("  Gesamt: %.2f \n",meineKnoten[i]->entfernungZumUrsprung);
+            }
+        }
+
+
+
+    }
+}
+
+
+void ladeDaten();//TODO Hier wird von dbio die daten geholt und in das struct geschrieben , wird schon beim öffnen des menus ausgeführt.
+
+int start()//TODO PARAMETER Verarbeitung und verknüpfung mit menu , Dateneingabe muss in ladedaten();
 {
 
-    //Karte mit Knoten und Wegen wird geladen
+    //Anzahl an Knoten wird festgelegt
+    AnzahlKnoten=10;
+    //Lege Startknoten Fest
+    int StartKnoten=5;
 
-    struct Karte meineKarte;
+    //Ein array mit "AnzahlKnoten" einträgen , aus Knoten wird erstellt und somit speicher zugewiesen
+    struct Knoten *meineKnoten[AnzahlKnoten];
 
-    //Speicher für die Knoten Belegen
+
+    //TESTDATEN VON http://www.geeksforgeeks.org/greedy-algorithms-set-6-dijkstras-shortest-path-algorithm/
+
+    //Es müssen "AnzahlKnoten" mal Knoten erstellt werden sonst kommt es zum Speicherzugriffsfehler parameter(PointerZumKnotenarray,int ID des Knotens, Name des Knotens)
+    setzeKnotenName(meineKnoten, 0, "Null");
+    setzeKnotenName(meineKnoten, 1, "Eins");
+    setzeKnotenName(meineKnoten, 2, "Zwei");
+    setzeKnotenName(meineKnoten, 3, "Drei");
+    setzeKnotenName(meineKnoten, 4, "Vier");
+    setzeKnotenName(meineKnoten, 5, "Fünf");
+    setzeKnotenName(meineKnoten, 6, "Sechs");
+    setzeKnotenName(meineKnoten, 7, "Sieben");
+    setzeKnotenName(meineKnoten, 8, "Acht");
+    setzeKnotenName(meineKnoten, 9, "N/R Knoten");
+
+    //Werte werden Initialisiert im KnotenArray , Dies muss vor dem erstellen der wege passieren da Numwege initialisiert wird.
     for (int i = 0; i < AnzahlKnoten; i++){
-       void *K = malloc(sizeof(struct Knoten));
-        meineKarte.Knoten[i]=K;
+        meineKnoten[i]->entfernungZumUrsprung = INT_MAX , meineKnoten[i]->besucht = false ,meineKnoten[i]->numWege=0;
+        meineKnoten[i]->knotenZurueck= malloc(sizeof(struct Knoten));
     }
 
+    //Hier werden die verbindungen zwischen den knoten erstellt , sie sind immmer bidirektional d.h. in beide richtungen ,die reihenfolge der knoten ist somit egal.
+    //parameter(PointerZumKnotenarray,int ID des ersten Knotens,int ID des zweiten Knotens,entfernung zwischen den knoten)
+    ErstelleWegBidirektional(meineKnoten,0,1,4);
+    ErstelleWegBidirektional(meineKnoten,0,7,8);
+    ErstelleWegBidirektional(meineKnoten,1,7,11);
+    ErstelleWegBidirektional(meineKnoten,1,2,8);
+    ErstelleWegBidirektional(meineKnoten,2,8,2);
+    ErstelleWegBidirektional(meineKnoten,2,3,7);
+    ErstelleWegBidirektional(meineKnoten,2,5,4);
+    ErstelleWegBidirektional(meineKnoten,3,4,9);
+    ErstelleWegBidirektional(meineKnoten,3,5,14);
+    ErstelleWegBidirektional(meineKnoten,4,5,10);
+    ErstelleWegBidirektional(meineKnoten,5,6,2);
+    ErstelleWegBidirektional(meineKnoten,6,8,6);
+    ErstelleWegBidirektional(meineKnoten,6,7,1);
+    ErstelleWegBidirektional(meineKnoten,7,8,7);
 
-    meineKarte = setzeKnotenName(meineKarte, 0, "Eins");
-    meineKarte = setzeKnotenName(meineKarte, 1, "Zwei");
-    meineKarte = setzeKnotenName(meineKarte, 2, "Drei");
-    meineKarte = setzeKnotenName(meineKarte, 3, "Vier");
+    //Algorythmus wird gestartet
+    dijkstra(meineKnoten, StartKnoten);
 
-    //restlichen Werte werden definiert (UNBEDINGT BEVOR WEGE VERKNÜPFT WERDEN DALLE NUMWEGE =0 GESETZT WERDEN
-    for (int i = 0; i < AnzahlKnoten; i++){
-        meineKarte.Knoten[i]->entfernungZumUrsprung = INT_MAX , meineKarte.Knoten[i]->besucht = false ,meineKarte.Knoten[i]->numWege=0;
-    }
-
-
-
-    meineKarte=ErstelleWegBidirektional(meineKarte,0,1,4);
-    meineKarte=ErstelleWegBidirektional(meineKarte,1,2,8);
-
-
-
-
-
-
-    dijkstra(meineKarte, 0);
+    //TODO Ausgaben schreiben , mit printui tabellen
+    printAllPaths(meineKnoten,StartKnoten);
 
     return 0;
 }
